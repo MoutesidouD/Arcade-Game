3e3f94e3a7bf2b2bf0a6f0263083d337fd893ae2
Dimitra <dmoutesidou@hotmail.com>
2018-06-29 12:27:46 +0300

    Make board appear

--- js/app.js
+++ js/app.js
@@ -21,12 +21,25 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player= function(x,y,sprite) {
+  this.x= x;
+  this.y= y;
+  this.sprite = 'images/char-pink-girl.png';
+};
 
+Player.prototype.update= function(){
 
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var player= new Player;
+var enemy= new Enemy;
+var allEnemies=[];
 
 
 // This listens for key presses and sends the keys to your
