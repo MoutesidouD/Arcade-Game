ae7f8522aaab49a53ba53965a55a719ef627fdeb
Dimitra <dmoutesidou@hotmail.com>
2018-07-02 00:53:17 +0300

    Set enemies on lanes and make them run

--- js/app.js
+++ js/app.js
@@ -10,11 +10,14 @@ var Enemy = function(x,y,speed) {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
 
-    // You should multiply any movement by the dt parameter
-    this.x= this.speed * dt;
-
-    // which will ensure the game runs at the same speed for
-    // all computers.
+/*You should multiply any movement by the dt parameters
+  which will ensure the game runs at the same speed for
+  all computers.*/
+    if (this.x < 505) {
+      this.x += this.speed * dt;
+    } else {
+      this.x = -100; //set enemy back to start once complete its run
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -22,49 +25,55 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Creates player
 var Player= function(x, y) {
   this.x= x;
   this.y= y;
   this.sprite= 'images/char-pink-girl.png';
 };
 
+//Updates player's position
 Player.prototype.update= function(){
 
 };
 
+//Draws player on screen
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function(key){ //relative values.need to be fixed
-  if(key ==='left' && this.x > 0){
+//Allows player move along board
+//and sets limits to avoid moving out of board
+Player.prototype.handleInput = function(allowedKeys){
+  if(allowedKeys ==='left' && this.x > 0){
     this.x -= 101;
   }
-  if(key === 'right' && this.x <400){
+  if(allowedKeys === 'right' && this.x <400){
     this.x += 101;
   }
-  if(key === 'up' && this.y > 0 ){
+  if(allowedKeys === 'up' && this.y > 0 ){
     this.y -= 83;
   }
-  if(key === 'down' && this.y < 400 ){
+  if(allowedKeys === 'down' && this.y < 400 ){
     this.y += 83;
   }
 
 }
 
-
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var player= new Player(202,405);
-var enemy1= new Enemy(20, 60,40);
-var enemy2= new Enemy(100,145,50);
-var enemy3= new Enemy(250,229,60);
-var allEnemies=[];
-allEnemies.push(enemy1, enemy2, enemy3);
+var player= new Player(202,405);// Place the player object in a variable called player
+var enemiesY= [63, 147, 230]; //Lanes(y) where enemies can run
+var enemy;
+var allEnemies=[]; // Place all enemy objects in an array called allEnemies
+
+enemiesY.forEach(function(y) {
+  //create enemies(off-screen) for each lane and their random speed
+    enemy = new Enemy(-101, y, 100 + Math.floor(Math.random() * 300));
+    allEnemies.push(enemy);
+});
+
+
+
 
 
 // This listens for key presses and sends the keys to your
